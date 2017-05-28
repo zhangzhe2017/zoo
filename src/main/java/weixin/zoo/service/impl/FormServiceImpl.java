25ca9c2d3041fcf86d1b466f22eb23202148a37b
viczhang.zhangz <viczhang.zhangz@alibaba-inc.com>
2017-05-28 23:47:43 +0800

    add by zz

--- src/main/java/weixin/zoo/service/impl/FormServiceImpl.java
+++ src/main/java/weixin/zoo/service/impl/FormServiceImpl.java
@@ -56,8 +56,9 @@ public class FormServiceImpl implements FormService {
                     while(itr.hasNext()){
                         String value = (String)itr.next();
                         String mediaLocalPath = WxServiceCenter.downLoadMediaSource(value);
-                        String str = mediaLocalPath.trim();
-                        String keyStr = str.substring(str.lastIndexOf("//")+1);
+                        //String str = mediaLocalPath.trim();
+                        //String keyStr = str.substring(str.lastIndexOf("//") + 1);
+                        String keyStr = String.valueOf(new Date().getTime() /1000);
                         boolean upload = OssUtils.uploadFile(mediaLocalPath, keyStr);
                         if(upload){
                             String url = OssUtils.getFileUrl(keyStr);
